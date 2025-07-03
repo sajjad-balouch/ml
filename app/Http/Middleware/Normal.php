986b19452cd4ff029f4b945e17faf13c8daa9279
Sajjad Baloch <70022001+sajjad-balouch@users.noreply.github.com>
2025-07-03 20:07:39 +0500

    Update Normal.php

--- app/Http/Middleware/Normal.php
+++ app/Http/Middleware/Normal.php
@@ -15,16 +15,10 @@ class Normal
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $user = auth()->user();
-        // if (!$user) {
-        //     // Redirect to login if not logged in
-        //     return redirect('/');
-        // }
-
-        if ($user->role == 1) {
-            // Redirect or abort if role is not admin
-            return redirect()->route('admin');
+        if (auth()->check() && auth()->user()->role == 0) {
+            return $next($request);
         }
-        return $next($request);
+
+        return redirect('/');
     }
 }
